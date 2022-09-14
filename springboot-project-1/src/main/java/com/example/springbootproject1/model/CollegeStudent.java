ec50b9d50e948779490306c8a18abd763fcdcb41
Sielei Herman <hsielei@gmail.com>
2022-09-14 14:56:39 +0300

    created a springboot project for testing springboot example

---
+++ springboot-project-1/src/main/java/com/example/springbootproject1/model/CollegeStudent.java
@@ -0,0 +1,24 @@
+package com.example.springbootproject1.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data @AllArgsConstructor @NoArgsConstructor @Builder
+public class CollegeStudent implements Student {
+    private String firstname;
+    private String lastname;
+    private String emailAddress;
+    private StudentGrades studentGrades;
+
+    @Override
+    public String studentInformation() {
+        return getFullName() + " " + getEmailAddress();
+    }
+
+    @Override
+    public String getFullName() {
+        return getFirstname() + " " + getLastname();
+    }
+}
---
+++ springboot-project-1/src/main/java/com/example/springbootproject1/model/Student.java
@@ -0,0 +1,8 @@
+package com.example.springbootproject1.model;
+
+public interface Student {
+
+    String studentInformation();
+
+    String getFullName();
+}
